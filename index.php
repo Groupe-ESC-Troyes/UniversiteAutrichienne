df378e5b5681bd37fc71bf067c5e1f7bd10bd1a7
Yannick Lohse <contact@yannick-lohse.fr>
2013-04-24 14:32:02 +0200

    Structure basique du contenu

--- index.php
+++ index.php
@@ -43,11 +43,81 @@
 <body>
 	<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
 	<header>
+		<h1>
+			3<sup>ème</sup> édition de l'Université d'Automne en Économie Autrichienne
+		</h1>
+		<h6>
+			du 4 au 6 octobre 2013 à Troyes
+		</h6>
 	</header>
-	<div role="main">
-	</div>
-	<footer>
-	</footer>
+	
+	<section>
+		<header>
+			<h2>
+				Présentation
+			</h2>
+		</header>
+		<p>
+			Pour la 3ème édition de l’université d’automne en économie autrichienne et compte tenu de la gravité de la crise financière et monétaire que nous vivons, les questions monétaires seront au cœur des débats et plus particulièrement la question des solutions et des réformes à mener seront privilégiées. 
+		</p>
+		<p>
+			Les organisateurs de l’évènement (voir programme), s’adressent à la fois aux étudiants et professionnels désireux de découvrir une série de raisonnements cohérents pour expliquer la complexité des enjeux économiques, et à ceux qui en connaissant déjà certains aspects, voudraient approfondir leurs connaissances et leur compréhension de la crise actuelle.
+		</p>
+	</section>
+	
+	<section>
+		<header>
+			<h2>
+				Programme
+			</h2>
+		</header>
+		
+		<h3>
+			Day one
+		</h3>
+		<table>
+			<tr>
+				<td>
+					6h45
+				</td>
+				<td>
+					Some content
+				</td>
+			</tr>
+			<tr>
+				<td>
+					6h45
+				</td>
+				<td>
+					Some more content
+				</td>
+			</tr>
+		</table>
+	</section>
+	
+	<section>
+		<header>
+			<h2>
+				Concours du meilleur article
+			</h2>
+		</header>
+		
+		<p>
+			Some content
+		</p>
+	</section>
+	
+	<section>
+		<header>
+			<h2>
+				Inscription
+			</h2>
+		</header>
+		
+		<p>
+			Formulaire
+		</p>
+	</section>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="/js/libs/jquery-1.9.1.min.js"><\/script>')</script>
